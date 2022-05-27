2d9c2262f241afce8291eb6495ac52cce838308c
Triyono <yono.tri@gmail.com>
2022-05-27 15:44:04 +0700

    penambahan fungsi parameter

---
+++ fungsi_parameter.py
@@ -0,0 +1,16 @@
+def selamat_datang(nama):
+
+    print('halo', nama, 'apa kabar?')
+
+
+# selamat_datang('Budi')
+
+
+def greetings(nama, waktu):
+    print('hello', nama, 'selamat', waktu)
+    print('apakah ', nama, 'sudah datang', waktu, 'ini?')
+
+
+greetings('andi', 'pagi')
+greetings('rudi', 'siang')
+greetings('wahyu', 'malam')
