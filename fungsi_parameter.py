7d1c40d7a42d26dd0b0483eaa51a32da8688b301
Triyono <yono.tri@gmail.com>
2022-05-27 16:00:00 +0700

    penambahan fungsi return

--- fungsi_parameter.py
+++ fungsi_parameter.py
@@ -10,6 +10,18 @@ def greetings(nama, waktu):
     print('hello', nama, 'selamat', waktu)
 
 
-greetings('malam', 'wahyu')  # positional parameter
+# greetings('malam', 'wahyu')  # positional parameter
 
-greetings(waktu='malam', nama='wahyu')  # named paramater
+# greetings(waktu='malam', nama='wahyu')  # named paramater
+
+def penjumlahan(a, b):
+    hasil = a+b
+    return hasil
+
+
+data_greeting = greetings('wahyu', 'pagi')
+
+hasil_jumlah = penjumlahan(2, 5)
+
+print(data_greeting)
+print(hasil_jumlah)
